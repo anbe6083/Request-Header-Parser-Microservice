df81eb3a61165de3a98239cfea4b41470327f940
anbe6083 <andrewberumen1991@gmail.com>
2017-11-08 12:56:19 -0800

    Revert "null"

--- server.js
+++ server.js
@@ -4,7 +4,6 @@
 // init project
 var express = require('express');
 var app = express();
-var os = require('os');
 
 // we've started you off with Express, 
 // but feel free to use whatever libs or frameworks you'd like through `package.json`.
@@ -14,17 +13,25 @@ app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (request, response) {
-  var ip = (request.header('x-forwarded-for') || request.connection.remoteAddress).split(",")[0];
-  var lang = request.headers['accept-language'].split(",")[0];
-  
-  var test = {
-    address: ip,
-    language: lang,
-    OS: os.platform()
-  };
-  response.json(test);
+  response.sendFile(__dirname + '/views/index.html');
 });
 
+app.get("/dreams", function (request, response) {
+  response.send(dreams);
+});
+
+// could also use the POST body instead of query string: http://expressjs.com/en/api.html#req.body
+app.post("/dreams", function (request, response) {
+  dreams.push(request.query.dream);
+  response.sendStatus(200);
+});
+
+// Simple in-memory store for now
+var dreams = [
+  "Find and count some sheep",
+  "Climb a really tall mountain",
+  "Wash the dishes"
+];
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
