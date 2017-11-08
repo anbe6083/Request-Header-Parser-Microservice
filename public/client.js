e8a7667603d7fe9f73ce26fe8db041e0e10815a1
Glitch (statuesque-motion) <none>
2017-11-08 20:53:04 +0000

    null

--- public/client.js
+++ public/client.js
@@ -6,21 +6,5 @@
 
 $(function() {
   console.log('hello world :o');
-  
-  $.get('/dreams', function(dreams) {
-    dreams.forEach(function(dream) {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-    });
-  });
-
-  $('form').submit(function(event) {
-    event.preventDefault();
-    var dream = $('input').val();
-    $.post('/dreams?' + $.param({dream: dream}), function() {
-      $('<li></li>').text(dream).appendTo('ul#dreams');
-      $('input').val('');
-      $('input').focus();
-    });
-  });
 
 });
